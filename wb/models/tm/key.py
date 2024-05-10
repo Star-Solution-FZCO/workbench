acafe29bbdcae021c89aafede77335ef99e498ac
BelCo <12194066-BelCo94@users.noreply.gitlab.com>
2024-05-10 05:14:07 +0200

    tm: store key bcrypt hash instead of key itself

--- wb/models/tm/key.py
+++ wb/models/tm/key.py
@@ -1,11 +1,14 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
 
+import bcrypt
 import sqlalchemy as sa
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from shared_utils.sql import EncryptedObject
-from wb.config import CONFIG
 from wb.db import BaseDBModel
+
+if TYPE_CHECKING:
+    from wb.models.employee import Employee
 
 __all__ = ('EmployeeTM',)
 
@@ -18,7 +21,18 @@ class EmployeeTM(BaseDBModel):
         ),
         primary_key=True,
     )
-    last_logon: Mapped[datetime | None]
-    key: Mapped[str] = mapped_column(
-        EncryptedObject['str'](str, passphrase=CONFIG.DB_ENCRYPT_KEY)
+    employee: Mapped['Employee'] = relationship(
+        foreign_keys=[employee_id], lazy='selectin', back_populates='tm'
     )
+    last_logon: Mapped[datetime | None]
+    key_hash: Mapped[str]
+
+    @staticmethod
+    def hash_key(key: str) -> str:
+        return bcrypt.hashpw(key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def set_key(self, key: str) -> None:
+        self.key_hash = self.hash_key(key)
+
+    def check_key(self, key: str) -> bool:
+        return bcrypt.checkpw(key.encode('utf-8'), self.key_hash.encode('utf-8'))
