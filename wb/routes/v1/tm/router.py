e0db729728dc0aff0e53aace52951c4d59f6f729
BelCo <12194066-BelCo94@users.noreply.gitlab.com>
2024-06-21 19:22:15 +0200

    back: add endpoint for getting last tm manual status

--- wb/routes/v1/tm/router.py
+++ wb/routes/v1/tm/router.py
@@ -24,6 +24,37 @@ __all__ = ('router',)
 router = APIRouter(prefix='/api/v1/tm', tags=['v1', 'tm'])
 
 
+@router.get('/status/{employee_id}/manual')
+async def get_employee_status_manual(
+    employee_id: int,
+    session: AsyncSession = Depends(get_db_session),
+) -> BasePayloadOutput[TMStatusOut]:
+    emp = await get_employee_by_id(employee_id, session=session)
+    if not emp:
+        raise HTTPException(HTTPStatus.NOT_FOUND, detail='employee not found')
+    last_log: m.TMRecord | None = await session.scalar(
+        sa.select(m.TMRecord)
+        .where(
+            m.TMRecord.employee_id == emp.id,
+            m.TMRecord.status.in_((m.TMRecordType.LEAVE, m.TMRecordType.COME)),
+            m.TMRecord.source != 'auto',
+        )
+        .order_by(m.TMRecord.time.desc())
+        .limit(1)
+    )
+    if not last_log:
+        payload = TMStatusOut(
+            status=m.TMRecordType.LEAVE,
+            updated=None,
+        )
+    else:
+        payload = TMStatusOut(
+            status=last_log.status,
+            updated=last_log.time,
+        )
+    return make_success_output(payload=payload)
+
+
 @router.get('/status/{employee_id}')
 async def get_employee_status(
     employee_id: int,
