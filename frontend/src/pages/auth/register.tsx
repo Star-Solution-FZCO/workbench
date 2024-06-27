4f08e1546b430aa0842a4ef7d1aca55db809a3b7
BelCo <12194066-BelCo94@users.noreply.gitlab.com>
2024-06-27 22:26:39 +0200

    local auth: show password generator on the registration page

--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -13,6 +13,7 @@ import {
 import { FormTextField, Title } from "_components";
 import { authActions } from "_redux";
 import { AUTH_MODE, passwordValidationRules } from "config";
+import PasswordGenerator from "pages/password_generator";
 import React, { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { useNavigate, useSearchParams } from "react-router-dom";
@@ -66,124 +67,134 @@ export const RegisterPage: React.FC = () => {
     };
 
     return (
-        <ThemeProvider theme={theme}>
-            <Title title="Registration" />
+        <>
+            <ThemeProvider theme={theme}>
+                <Title title="Registration" />
 
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
 
-                    <Typography component="h1" variant="h5">
-                        Registration
-                    </Typography>
+                        <Typography component="h1" variant="h5">
+                            Registration
+                        </Typography>
 
-                    <Box sx={{ mt: 1 }}>
-                        <FormProvider {...methods}>
-                            <form
-                                onSubmit={methods.handleSubmit(
-                                    handleSubmit,
-                                    () => {},
-                                )}
+                        <Box sx={{ mt: 1 }}>
+                            <FormProvider {...methods}>
+                                <form
+                                    onSubmit={methods.handleSubmit(
+                                        handleSubmit,
+                                        () => {},
+                                    )}
+                                >
+                                    <Grid container spacing={1}>
+                                        <Grid item xs={12}>
+                                            <FormControl
+                                                sx={{ m: 1 }}
+                                                fullWidth
+                                            >
+                                                <FormTextField
+                                                    name="register_token"
+                                                    label="Registration token"
+                                                    register={methods.register}
+                                                    rules={genRules({
+                                                        required: true,
+                                                    })}
+                                                    // @ts-ignore
+                                                    errors={
+                                                        methods.formState.errors
+                                                    }
+                                                />
+                                            </FormControl>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <FormControl
+                                                sx={{ m: 1 }}
+                                                fullWidth
+                                            >
+                                                <FormTextField
+                                                    name="password"
+                                                    label="New password"
+                                                    register={methods.register}
+                                                    type="password"
+                                                    rules={genRules({
+                                                        required: true,
+                                                        minLength: 11,
+                                                    })}
+                                                    // @ts-ignore
+                                                    errors={
+                                                        methods.formState.errors
+                                                    }
+                                                />
+                                            </FormControl>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <FormControl
+                                                sx={{ m: 1 }}
+                                                fullWidth
+                                            >
+                                                <FormTextField
+                                                    name="password2"
+                                                    label="Repeat password"
+                                                    register={methods.register}
+                                                    type="password"
+                                                    rules={genRules({
+                                                        required: true,
+                                                        minLength: 11,
+                                                    })}
+                                                    // @ts-ignore
+                                                    errors={
+                                                        methods.formState.errors
+                                                    }
+                                                />
+                                            </FormControl>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <LoadingButton
+                                                fullWidth
+                                                variant="contained"
+                                                sx={{ mt: 3, mb: 2, m: 1 }}
+                                                loading={isLoading}
+                                                type="submit"
+                                            >
+                                                Submit
+                                            </LoadingButton>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </FormProvider>
+
+                            <Box
+                                display={"flex"}
+                                justifyContent={"space-between"}
                             >
-                                <Grid container spacing={1}>
-                                    <Grid item xs={12}>
-                                        <FormControl sx={{ m: 1 }} fullWidth>
-                                            <FormTextField
-                                                name="register_token"
-                                                label="Registration token"
-                                                register={methods.register}
-                                                rules={genRules({
-                                                    required: true,
-                                                })}
-                                                // @ts-ignore
-                                                errors={
-                                                    methods.formState.errors
-                                                }
-                                            />
-                                        </FormControl>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <FormControl sx={{ m: 1 }} fullWidth>
-                                            <FormTextField
-                                                name="password"
-                                                label="New password"
-                                                register={methods.register}
-                                                type="password"
-                                                rules={genRules({
-                                                    required: true,
-                                                    minLength: 11,
-                                                })}
-                                                // @ts-ignore
-                                                errors={
-                                                    methods.formState.errors
-                                                }
-                                            />
-                                        </FormControl>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <FormControl sx={{ m: 1 }} fullWidth>
-                                            <FormTextField
-                                                name="password2"
-                                                label="Repeat password"
-                                                register={methods.register}
-                                                type="password"
-                                                rules={genRules({
-                                                    required: true,
-                                                    minLength: 11,
-                                                })}
-                                                // @ts-ignore
-                                                errors={
-                                                    methods.formState.errors
-                                                }
-                                            />
-                                        </FormControl>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <LoadingButton
-                                            fullWidth
-                                            variant="contained"
-                                            sx={{ mt: 3, mb: 2, m: 1 }}
-                                            loading={isLoading}
-                                            type="submit"
-                                        >
-                                            Submit
-                                        </LoadingButton>
-                                    </Grid>
-                                </Grid>
-                            </form>
-                        </FormProvider>
-
-                        <Box display={"flex"} justifyContent={"space-between"}>
-                            <Typography variant="body2">
-                                <a href={"/login"}>Login</a>
-                            </Typography>
-                            <Typography variant="body2">
-                                <a href={"/password-generator"}>
-                                    Password generator
-                                </a>
-                            </Typography>
+                                <Typography variant="body2">
+                                    <a href={"/login"}>Login</a>
+                                </Typography>
+                            </Box>
+                            <ul>
+                                <Typography variant={"body2"} sx={{ m: 1 }}>
+                                    Password requirements:
+                                    {passwordValidationRules.map((rule) => (
+                                        <li key={rule}>{rule}</li>
+                                    ))}
+                                </Typography>
+                            </ul>
                         </Box>
-                        <ul>
-                            <Typography variant={"body2"} sx={{ m: 1 }}>
-                                Password requirements:
-                                {passwordValidationRules.map((rule) => (
-                                    <li key={rule}>{rule}</li>
-                                ))}
-                            </Typography>
-                        </ul>
                     </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
+                </Container>
+            </ThemeProvider>
+            <PasswordGenerator />
+        </>
     );
 };
